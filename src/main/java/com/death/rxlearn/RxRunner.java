b22d127d78d6d06f28b21a16bea2bb8cf3b57e0c
deathcode <carotkut12@gmail.com>
2019-03-10 19:49:23 +0530

    Added few operator use cases with sample code

--- src/main/java/com/death/rxlearn/RxRunner.java
+++ src/main/java/com/death/rxlearn/RxRunner.java
@@ -11,7 +11,10 @@ import java.util.List;
 public class RxRunner {
 
     static int result = 0;
+    static String[] titleList = {"Lord", "Mount", "Baton"};
     public static void main(String[] args) {
+
+
 
         /**
          * A Simple observable and subscriber
@@ -64,5 +67,51 @@ public class RxRunner {
         );
         // Here we can see a huge line of code reduction and it makes it more
         // Readable
+
+        /*
+        Now lets do some transformation to data
+         */
+
+        //TODO: Here we are going to use in the Map operator and will change the case of letters in array
+
+        Observable.fromArray(letters)
+                .map(String::toUpperCase)
+                .subscribe(System.out::println);
+
+        //TODO: Lets use FlatMap to flatten the nested observable
+        Observable.just("book1", "book2")
+                .flatMap(s->getTitle())
+                .subscribe(System.out::println);
+
+        // Here for two items book1 and book2 flatMap will be called for both of them
+        // and it will call in the getTitle which return observable
+        // and then this will call in the onNext of subscribe with the data
+        // and will print Lord\nMount\Baton 2 times, depending upon the number of items in
+        // just(...)
+
+        //TODO: Lets use the groupBy operator to group the numbers in even or odd
+        List<Integer> n = Arrays.asList(1,2,3,4,5,6,7,8);
+        Observable.fromIterable(n)
+                .groupBy(num->0==(num%2)?"EVEN":"ODD")
+                .subscribe(
+                        group->group.subscribe(
+                                (data)->{
+                                    if(group.getKey().toString().equals("EVEN")){
+                                        System.out.println("EVEN "+data);
+                                    }else{
+                                        System.out.println("ODD "+ data);
+                                    }
+                                }
+                        )
+                );
+
+        //TODO: Lets filter out the even numbers and print them
+        Observable.fromIterable(n)
+                .filter(i->(i%2==0))
+                .subscribe(System.out::println);
+    }
+
+    static Observable<String> getTitle(){
+        return Observable.fromArray(titleList);
     }
 }
